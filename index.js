cc06929415dea78b15a27eeb82684a8d72112540
Abdul-Kader-Rimon <abdulkaderrimon8@gmail.com>
2026-04-27 18:35:10 +0600

    vercel.json file added

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server started on port ${PORT}`);
+// });
+
+module.exports = app;
